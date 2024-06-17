54da66a32e962f39ad91c7f3e8ebef7a397daa18
Lien Gabi <Lien.nguyen@crewlounge.aero>
2024-06-17 16:25:31 +0700

    BT1

---
+++ BT_JAVA_BASIC_1/SinhVien.java
@@ -0,0 +1,30 @@
+package BT_JAVA_BASIC_1;
+
+public class SinhVien {
+ /*
+    //Bien Cuc Bo
+    public void KhoaCntt(){
+        String tenLop = "K61CNTTA";
+        int soLuong = 50;
+        System.out.println("Ten lop cua ban: " + tenLop);
+        System.out.println("So luong sinh vien la: " + soLuong);
+    }
+  */
+  /*
+    //Bien Toan Cuc
+     String tenLop = "K61CNTTA";
+    int soLuong = 50;
+
+       public static void main(String[] args) {
+        SinhVien sv = new SinhVien();
+        System.out.println("Ten lop cua ban: "+sv.tenLop);
+        System.out.println("So luong sinh vien la: "+sv.soLuong);
+    }
+    */
+    /*
+    //Bien static
+    public static String tenLop = "K61CNTTA";
+    public static  int soLuong = 50;
+
+     */
+}
---
+++ BT_JAVA_BASIC_1/ThongTin.java
@@ -0,0 +1,9 @@
+/*package BT_JAVA_BASIC_1;
+
+public class ThongTin {
+    public static void main(String[] args) {
+        System.out.println("Ten lop cua ban: "+SinhVien.tenLop);
+        System.out.println("So luong sinh vien la: "+SinhVien.soLuong);
+    }
+}
+*/
